4bf4d97caf3e38e68cc41e2cdff74e4f7280d83f
angielyn.rivera <angielyn.rivera@SVPHLP00674>
2022-06-27 05:37:06 +0800

    Added item controllers, error controllers & item services file.

---
+++ ASPNetCoreMastersToDoList/Services/ItemService.cs
@@ -0,0 +1,11 @@
+﻿namespace Services
+{
+    public static class ItemService
+    {
+        public static IEnumerable<string> GetAll(int userId)
+        {
+            return new List<string>(userId);
+        }
+
+    }
+}
