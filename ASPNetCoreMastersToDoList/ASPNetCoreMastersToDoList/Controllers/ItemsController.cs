d57a9b3eefc01c740c16d47945abba4fdfd28ea8
angielyn.rivera <angielyn.rivera@SVPHLP00674>
2022-06-28 12:58:11 +0800

    Update files

---
+++ ASPNetCoreMastersToDoList/ASPNetCoreMastersToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,21 @@
+﻿using ASPNetCoreMastersToDoList.BindingModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace ASPNetCoreMastersToDoList.Controllers
+{
+    public class ItemsController : ControllerBase
+    {
+        public int Get(int userId)
+        {
+            ItemService.GetAll();
+            return userId;
+        }
+
+        public void Post(ItemCreateBindingModel createModel)
+        {
+            //mapped to an itemDTO for the iteam service save method to consume
+        }
+    }
+}
---
+++ ASPNetCoreMastersToDoList/Services/ItemService.cs
@@ -0,0 +1,14 @@
+﻿namespace Services
+{
+    public static class ItemService
+    {
+        public static void GetAll()
+        {
+
+        }
+        public static void Save()
+        {
+
+        }
+    }
+}
