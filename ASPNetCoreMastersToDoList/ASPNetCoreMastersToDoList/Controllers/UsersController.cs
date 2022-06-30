e2bbffd03858e3e63f517738f26d11762023e61d
angielyn.rivera <angielyn.rivera@SVPHLP00674>
2022-06-30 19:51:36 +0800

    Created session6 branch

---
+++ ASPNetCoreMastersToDoList/ASPNetCoreMastersToDoList/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNetCoreMastersToDoList.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Login()
+        {
+
+        }
+    }
+}
