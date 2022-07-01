0ad4a9abcd72f8731d74c245351690a19c70becc
angielyn.rivera <angielyn.rivera@SVPHLP00674>
2022-07-01 13:10:51 +0800

    Updated files.

--- ASPNetCoreMastersToDoList/Repositories/DataContext.cs
+++ ASPNetCoreMastersToDoList/Repositories/DataContext.cs
@@ -10,5 +10,30 @@ namespace Repositories
     public class DataContext
     {
         public List<Item> Items;
+
+        public DataContext()
+        {
+            this.Items = new List<Item>();
+
+            var rnd = new Random();
+            Items.Add(new Item()
+            {
+                Id = 1,
+                Text = "Hurer",
+
+            });
+            Items.Add(new Item()
+            {
+                Id = 2,
+                Text = "fgfg",
+
+            });
+            Items.Add(new Item()
+            {
+                Id = 3,
+                Text = "fgfdg",
+
+            });
+        }
     }
 }
