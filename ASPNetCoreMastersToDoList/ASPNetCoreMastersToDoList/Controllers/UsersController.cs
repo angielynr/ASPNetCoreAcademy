5bd7545b89a323b213370def3999a13b95348119
angielyn.rivera <angielyn.rivera@SVPHLP00674>
2022-07-01 22:47:32 +0800

    Updated Files. Final

--- ASPNetCoreMastersToDoList/ASPNetCoreMastersToDoList/Controllers/UsersController.cs
+++ ASPNetCoreMastersToDoList/ASPNetCoreMastersToDoList/Controllers/UsersController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASPNetCoreMastersToDoList.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace ASPNetCoreMastersToDoList.Controllers
 {
-    public class UsersController : Controller
+    [Route("users")]
+    [ApiController]
+    public class UsersController : ControllerBase
     {
-        //public IActionResult Login()
-        //{
+        private readonly Authentication _authentication;
 
-        //}
+        public UsersController(IOptions<Authentication> options)
+        {
+            _authentication = options.Value;
+            var securityKey = _authentication.SecurityKey;
+            var issuer = _authentication.Issuer;
+            var audience = _authentication.Audience;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return Ok(_authentication.SecurityKey);
+        }
     }
 }
